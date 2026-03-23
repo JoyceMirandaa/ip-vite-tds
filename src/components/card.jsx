61dca6ae96ca1f9d21b4b308056544bbe5a91ed1
JoyceMirandaa <biorajoycem@gmail.com>
2026-03-23 16:27:41 -0300

    Add files via upload

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,9 +1,9 @@
 import s from './card.module.css'
 
-export const Card = (imgScr, alt, title, text) => {
+export const Card = ({imgSrc, alt, title, text}) => {
     return (
         <div className={s.card}>
-            <img src={imgScr} alt={alt} />
+            <img src={imgSrc} alt={alt} />
             <h2>{title}</h2>
             <p>{text}</p>
         </div>
